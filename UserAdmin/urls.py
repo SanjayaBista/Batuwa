efbbda7f9f9da7b3bf1086bd069d6e81d4e34e13
SanjayaBista <sanjaybista681@gmail.com>
2022-06-17 16:19:03 +0545

    admin static

--- UserAdmin/urls.py
+++ UserAdmin/urls.py
@@ -7,6 +7,24 @@ urlpatterns = [
     path('categories/',views.categories, name='categories'),
     path('users/',views.users, name='users'),
     path('providers/',views.providers, name='providers'),
+    path('projects/',views.projects, name='projects'),
+    path('reports/',views.reports, name='reports'),
+    path('fees/',views.fees, name='fees'),
+    path('taxs/',views.taxs, name='taxs'),
+    path('roles/',views.roles, name='roles'),
+    path('rolesPermission/',views.rolesPermission, name='rolesPermission'),
+    path('skills/',views.skills, name='skills'),
+    path('verifyIdentity/',views.verifyIdentity, name='verifyIdentity'),
+    path('profiles/',views.profiles, name='profiles'),
+    path('settings/',views.settings, name='settings'),
+    path('localization/',views.localization, name='localization'),
+    path('paymentSetting/',views.paymentSetting, name='paymentSetting'),
+    path('emailSetting/',views.emailSetting, name='emailSetting'),
+    path('socialMedia/',views.socialMedia, name='socialMedia'),
+    path('socialLinks/',views.socialLinks, name='socialLinks'),
+    path('seoSettings/',views.seoSettings, name='seoSettings'),
+    path('others/',views.others, name='others'),
+    
 
 ]
 
--- UserAdmin/views.py
+++ UserAdmin/views.py
@@ -12,3 +12,54 @@ def users(request):
 
 def providers(request):
     return render(request, 'providers.html')
+
+def projects(request):
+    return render(request, 'projects.html')
+
+def reports(request):
+    return render(request, 'reports.html')
+
+def fees(request):
+    return render(request, 'fees.html')
+
+def taxs(request):
+    return render(request, 'taxs.html')
+
+def roles(request):
+    return render(request, 'roles.html')
+
+def rolesPermission(request):
+    return render(request, 'rolesPermissions.html')
+
+def skills(request):
+    return render(request, 'skills.html')
+
+def verifyIdentity(request):
+    return render(request, 'verifyIdentity.html')
+
+def profiles(request):
+    return render(request, 'profiles.html')
+
+def settings(request):
+    return render(request, 'settings.html')
+
+def localization(request):
+    return render(request, 'localization.html')
+
+def paymentSetting(request):
+    return render(request, 'paymentSetting.html')
+
+def emailSetting(request):
+    return render(request, 'emailSetting.html')
+
+def socialMedia(request):
+    return render(request, 'socialMedia.html')
+
+def socialLinks(request):
+    return render(request, 'socialLinks.html')
+
+def seoSettings(request):
+    return render(request, 'seoSettings.html')
+
+def others(request):
+    return render(request, 'others.html')
